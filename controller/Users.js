08e5ec22bba6915f0d0cf2e44bd06aae81ef8c0e
alangim <82983554+kartucho@users.noreply.github.com>
2022-07-24 20:11:52 -0300

    agrago Users

---
+++ controller/Users.js
@@ -0,0 +1,66 @@
+const { v4: uuidv4 } = require("uuid");
+const middleware = require('../utils/middleware');
+
+const router = require("express").Router();
+let dao = require("../dataccess/usuarios");
+
+/* Obtener todo */
+router.get("/", (req, res) => {
+    res.status(200).json(dao.getTodos(req.query));
+});
+
+/* Obtener uno especifico */
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    const data = dao.getUno(id);
+    if (data) {
+        res.status(200).json(data);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+
+// POST funcionando sin usuario logueado
+router.post("/", (req, res) => {
+
+    const body = {...req.body, id: middleware.getRandomInt(1, 1000000) };
+    dao.guardar(body);
+    res.status(200).json(body);
+});
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+
+    if (dao.borrar(id)) {
+        res.sendStatus(202);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+
+/* Modificar un elemento */
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+
+    if (dao.editar(id, req.body)) {
+        res.sendStatus(202);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+/* Modificar un elemento */
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+
+    if (dao.edit(id, req.body)) {
+        res.sendStatus(202);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+
+module.exports = router;
---
+++ dataccess/Users.js
@@ -0,0 +1,88 @@
+let Users = [{
+        id: 1,
+        nombre: "Alan",
+        apellido: "Gimelstein",
+        mail: "alan@gmail.com",
+        pass: ""
+    },
+    {
+        id: 2,
+        nombre: "Agustin",
+        apellido: "Basualdo",
+        mail: "agustin@gmail.com",
+        pass: ""
+    },
+    {
+        id: 3,
+        nombre: "Matias",
+        apellido: "Giusti",
+        mail: "matias@gmail.com",
+        pass: ""
+    },
+    {
+        id: 4,
+        nombre: "Gonzalo",
+        apellido: "Garcia",
+        mail: "gonzalo@gmail.com",
+        pass: ""
+    },
+
+];
+
+const getTodos = (filter) => {
+    let filtrar = Users;
+    if (filter.nombre) {
+        filtrar = filtrar.filter(e => e.nombre === filter.nombre);
+    }
+    if (filter.apellido) {
+        filtrar = filtrar.filter(e => e.apellido.includes(filter.apellido));
+    }
+
+    if (filter.mail) {
+        filtrar = filtrar.filter(e => email.includes(filter.mail));
+    }
+    if (filter.pass) {
+        filtrar = pass.filter(e => e.pass.includes(filter.pass));
+    }
+    if (filter.multinombres) {
+        filtrar = filtrar.filter(e => filter.multinombres.split(',').includes(e.nombre));
+    }
+    if (filter.buscar) {
+        filtrar = filtrar.filter(e => e.nombre.includes(filter.buscar) || e.apellido.includes(filter.buscar) || e.mail.includes(filter.buscar) || e.pass.includes(filter.buscar));
+    }
+
+
+    if (filter.multisearch) {
+        const palabrasABuscar = filter.multisearch.split(',');
+        filtrado = filtrado.filter(entry => {
+            const filtro = palabrasABuscar.filter(palabra => p.nombre.includes(palabra) || p.mail.includes(palabra))
+            return filtro.length > 0
+        })
+    }
+
+    return filtrar
+}
+
+const getUno = (id) => { return Users.find((registro) => registro.id == id); }
+const guardar = (body) => { Users.push(body); }
+
+const borrar = (id) => {
+    const index = Users.findIndex((registro) => registro.id == id);
+    if (index > 0) {
+        Users.splice(index, 1);
+        return true;
+    }
+    return false;
+}
+const editar = (id, body) => {
+    const index = Users.findIndex((registro) => registro.id == id);
+    if (index >= 0) {
+        Users[index] = body;
+        return true
+    }
+    return false
+}
+
+
+
+module.exports = { getTodos, getUno, guardar, borrar, editar };
