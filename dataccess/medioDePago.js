7c364e600c0217bfb082b340afe095e40f569a55
Aguz7in <basualdoagustin27@gmail.com>
2022-07-28 22:10:46 -0300

    medio de pago

---
+++ dataccess/medioDePago.js
@@ -0,0 +1,51 @@
+let Entry = [{
+    id: 1,
+    title: "Debito",
+},
+{
+    id: 2,
+    title: "Credito",
+},
+{
+    id: 3,
+    title: "Efectivo",
+},
+];
+
+const getAll = (filter) => {
+let filtrado = Entry;
+
+if (filter.title) {
+    filtrado = filtrado.filter(e => e.title === filter.title)
+}
+
+if (filter.multitle) {
+    filtrado = filtrado.filter(e => filter.multitle.split(',').includes(e.title))
+}
+
+return filtrado
+};
+
+const getOne = (id) => { return Entry.find((registro) => registro.id == id); }
+
+const save = (body) => { Entry.push(body); }
+
+const borrar = (id) => {
+const index = Entry.findIndex((registro) => registro.id == id);
+if (index > 0) {
+    Entry.splice(index, 1);
+    return true
+}
+return false
+}
+
+const update = (id, body) => {
+const index = Entry.findIndex((registro) => registro.id == id);
+if (index >= 0) {
+    Entry[index] = body;
+    return true
+}
+return false
+}
+
+module.exports = { getAll, getOne, save, borrar, update };
