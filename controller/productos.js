53f682770ac60168e8bbe2d08c12be40ce47f27c
ElBarbaGarcia <gondamgar@hotmail.com>
2022-07-31 00:22:58 -0300

    nos pones un 10?

--- controller/productos.js
+++ controller/productos.js
@@ -3,12 +3,28 @@ const middleware = require('../utils/middleware');
 const router = require("express").Router();
 let dao  = require("../dataccess/Productos");
 
-/* Obtener todo */
+// Obtener todo 
 router.get("/", (req, res) => { 
   res.status(200).json(dao.getAll(req.query));
 });
 
-/* Obtener uno especifico */
+
+//Obtener los destacados
+router.get("/destacados", (req, res) => {
+  res.status(200).json(dao.getDestacados());
+});
+
+//Obtener por categoria
+router.get("/categoria", (req, res) => {
+  res.status(200).json(dao.getCategoria(req.query.categoria));
+});
+
+//Obtener por marca
+router.get("/marca", (req, res) => {
+  res.status(200).json(dao.getMarca(req.query.marca));
+});
+
+// Obtener uno especifico 
 router.get("/:id", (req, res) => {
   const id = req.params.id;
   const data = dao.getOne(id);
@@ -20,16 +36,16 @@ router.get("/:id", (req, res) => {
   }
 });
 
-/* Agregar un elemento */
-//POST funcionando sin usuario logueado
-router.post("/", (req, res) => {
+// Agregar un elemento 
+//POST usuario logueado
+router.post("/", middleware.validarUserLogin, (req, res) => {
   
-  const body = { ...req.body, id: middleware.getRandomInt(1, 1000000)};
-  dao.save(body);
+  const body = {  id: uuidv4(), ...req.body};
   res.status(200).json(body);
 });
 
-router.delete("/:id", (req, res) => {
+// Borrar elemento
+router.delete("/:id",middleware.validarUserLogin, (req, res) => {
   const id = req.params.id;  
 
   if (dao.borrar(id)) { 
@@ -39,8 +55,8 @@ router.delete("/:id", (req, res) => {
   }
 });
 
-/* Modificar un elemento */
-router.put("/:id", (req, res) => {
+// Modificar un elemento 
+router.put("/:id",middleware.validarUserLogin, (req, res) => {
   const id = req.params.id;
  
   if (dao.update(id, req.body) ) { 
